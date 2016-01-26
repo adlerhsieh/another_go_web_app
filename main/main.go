ab34dce5612d259e190666139b2e8baf5b9ba068
Adler <nkj20932@hotmail.com>
2016-01-27 01:40:31 +0800

    use buffer to respond content to decrease memory usage

--- main/main.go
+++ main/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"io/ioutil"
+	"bufio"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -12,9 +13,13 @@ type MyHandler struct {
 
 func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	path := "./public" + req.URL.Path
-	data, err := ioutil.ReadFile(path)
+	// The following line reads the requested file directly, which takes huge memory usage
+	// Instead, we use buffer to prevent it. os simply opens the file and read it only when budio sends it to a reader
+	// data, err := ioutil.ReadFile(path)
+	f, err := os.Open(path)
 
 	if err == nil {
+		bufferedReader := bufio.NewReader(f)
 		var ContentType string
 		if strings.HasSuffix(path, ".css") {
 			ContentType = "text/css"
@@ -28,7 +33,8 @@ func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 			ContentType = "text/plain"
 		}
 		w.Header().Add("Content Type", ContentType)
-		w.Write(data)
+		bufferedReader.WriteTo(w)
+		// w.Write(data)
 	} else {
 		w.WriteHeader(404)
 		w.Write([]byte("404 - " + http.StatusText(404)))
