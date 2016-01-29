585587d728738ea6816f488c0b7f161f7fca1ced
Adler <nkj20932@hotmail.com>
2016-01-29 10:51:22 +0800

    add loop and sub-template

--- main/template.go
+++ main/template.go
@@ -8,28 +8,48 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Add("Content Type", "text/html")
-		tmpl, err := template.New("test").Parse(doc)
-		context := Context{"A Title", "John"}
-		if err == nil {
-			tmpl.Execute(w, context)
+		templates := template.New("template")
+		templates.New("doc").Parse(doc)
+		templates.New("header").Parse(header)
+		context := Context{
+			"Google",
+			"John",
+			[3]string{"Apple", "Lemon", "Orange"},
 		}
+		templates.Lookup("doc").Execute(w, context)
 	})
 	http.ListenAndServe(":8000", nil)
 }
 
+// For all template logic see https://golang.org/pkg/text/template/
 const doc = `
 <!DOCTYPE html>
 <html>
-  <head>
-	  <title>A {{.Title}}</title>
-	</head>
+{{template "header" .Title}}
 	<body>
 	  <h1>Hi, {{.Name}}</h1>
+		<h3>The fruits are:</h3>
+		<ul>
+			{{range .Fruit}}
+			  <li>{{.}}</li>
+			{{end}}
+		</ul>
 	</body>
 </html>
+`
+
+const header = `
+  <head>
+	  {{if eq . "Google"}}
+			<title>This is Google</title>
+		{{else}}
+			<title>A {{.}}</title>
+		{{end}}
+	</head>
 `
 
 type Context struct {
 	Title string
 	Name  string
+	Fruit [3]string
 }
