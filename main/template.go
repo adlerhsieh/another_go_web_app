74479e7eafdf709c7c1531943563590601faa8e7
Adler <nkj20932@hotmail.com>
2016-01-29 09:26:24 +0800

    add variables into template

--- main/template.go
+++ main/template.go
@@ -9,8 +9,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Add("Content Type", "text/html")
 		tmpl, err := template.New("test").Parse(doc)
+		context := Context{"A Title", "John"}
 		if err == nil {
-			tmpl.Execute(w, nil)
+			tmpl.Execute(w, context)
 		}
 	})
 	http.ListenAndServe(":8000", nil)
@@ -20,10 +21,15 @@ const doc = `
 <!DOCTYPE html>
 <html>
   <head>
-	  <title>A Title</title>
+	  <title>A {{.Title}}</title>
 	</head>
 	<body>
-	  <h1>Hi</h1>
+	  <h1>Hi, {{.Name}}</h1>
 	</body>
 </html>
 `
+
+type Context struct {
+	Title string
+	Name  string
+}
