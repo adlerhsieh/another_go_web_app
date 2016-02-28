17e8704084255c91399c138a219f2f34abe04966
Adler <nkj20932@hotmail.com>
2016-02-28 14:01:41 +0800

    remove unnecessary files & rename view.go to main.go

--- main/main.go
+++ main/main.go
@@ -1,58 +1,39 @@
 package main
 
 import (
-	"bufio"
+	// "fmt"
 	"net/http"
 	"os"
-	"strings"
+	"text/template"
+  "github.com/adlerhsieh/web/controllers"
 )
 
-type MyHandler struct {
-	http.Handler
-}
-
-func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	path := "./public" + req.URL.Path
-	// The following line reads the requested file directly, which takes huge memory usage
-	// Instead, we use buffer to prevent it. os simply opens the file and read it only when budio sends it to a reader
-	// data, err := ioutil.ReadFile(path)
-	f, err := os.Open(path)
-
-	if err == nil {
-		bufferedReader := bufio.NewReader(f)
-		var ContentType string
-		if strings.HasSuffix(path, ".css") {
-			ContentType = "text/css"
-		} else if strings.HasSuffix(path, ".html") {
-			ContentType = "text/html"
-		} else if strings.HasSuffix(path, ".js") {
-			ContentType = "application/javascript"
-		} else if strings.HasSuffix(path, ".png") {
-			ContentType = "image/png"
-		} else {
-			ContentType = "text/plain"
-		}
-		w.Header().Add("Content Type", ContentType)
-		bufferedReader.WriteTo(w)
-		// w.Write(data)
-	} else {
-		w.WriteHeader(404)
-		w.Write([]byte("404 - " + http.StatusText(404)))
-	}
-}
-
 func main() {
-	// http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-	// 	w.Write([]byte("Hello World"))
-	// })
-
-	// The following custom handler is to replace the above basic HandleFunc
-	http.Handle("/", new(MyHandler))
-
-	// nil means no multiplexing
+	templates := populateTemplates()
+	// Move http.HandleFunc() to controllers
+	controllers.Register(templates)
 	http.ListenAndServe(":8000", nil)
+}
 
-	// The fileserver can be served using built-in fileserver
-	// Then our MyHandler can be removed
-	// http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	basePath := "templates"
+	// open and close dir
+	templateFolder, _ := os.Open(basePath)
+	defer templateFolder.Close()
+
+	// -1 means every file
+	templatePathsRaw, _ := templateFolder.Readdir(-1)
+
+	// read every file and append filepath to a slice
+	templatePaths := new([]string)
+	for _, pathInfo := range templatePathsRaw {
+		if !pathInfo.IsDir() {
+			*templatePaths = append(*templatePaths, basePath+"/"+pathInfo.Name())
+		}
+	}
+
+	// ... at the end will brings in all values in a slice as args
+	result.ParseFiles(*templatePaths...)
+	return result
 }
